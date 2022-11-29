f66c4d369db29d82cdacfb2cf0a6067abc0cc1ff
Anthony Campolo <admin@ajcwebdev.com>
2022-11-29 11:23:36 -0600

    aave flash loan

---
+++ ethereum/aave-flash-loan/FlashLoan.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.6.6;
+import "./FlashLoanReceiverBase.sol";
+import "./ILendingPoolAddressesProvider.sol";
+import "./ILendingPool.sol";
+
+contract FlashloanV1 is FlashLoanReceiverBaseV1 {
+    constructor(address _addressProvider) FlashLoanReceiverBaseV1(_addressProvider) public{}
+
+    // Flash loan 1000000000000000000 wei (1 ether) worth of `_asset`
+    function flashloan(address _asset) public onlyOwner {
+        bytes memory data = "";
+        uint amount = 1 ether;
+
+        ILendingPoolV1 lendingPool = ILendingPoolV1(addressesProvider.getLendingPool());
+        lendingPool.flashLoan(address(this), _asset, amount, data);
+    }
+
+    // This function is called after your contract has received the flash loaned amount
+    function executeOperation(
+        address _reserve,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _params
+    )
+        external
+        override
+    {
+        require(_amount <= getBalanceInternal(address(this), _reserve), "Invalid balance, was the flashLoan successful?");
+
+        // Your logic goes here.
+        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
+
+        uint totalDebt = _amount.add(_fee);
+        transferFundsBackToPoolInternal(_reserve, totalDebt);
+    }
+}
---
+++ ethereum/aave-flash-loan/README.md
@@ -0,0 +1,12 @@
+# Make a Flash Loan using Aave
+
+This project is based on the guide, [How to Make a Flash Loan using Aave](https://www.quicknode.com/guides/defi/how-to-make-a-flash-loan-using-aave) by Noah Hein.
+
+## Clone Example Monorepo
+
+To begin, clone the `qn-guide-examples` repo and navigate to this project's directory.
+
+```bash
+git clone https://github.com/quiknode-labs/qn-guide-examples.git
+cd qn-guide-examples/ethereum/aave-flash-loan
+```
