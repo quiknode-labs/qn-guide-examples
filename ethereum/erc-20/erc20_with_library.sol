9aa776b4a1582c5157657aef016c24d0c752d0aa
Sahil sen <sensahil25@gmail.com>
2022-12-02 02:30:47 +0530

    Update erc20_with_library.sol

--- ethereum/erc-20/erc20_with_library.sol
+++ ethereum/erc-20/erc20_with_library.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+    constructor(uint256 initialSupply) ERC20("QuickNode Coin", "QKC") {
         _mint(msg.sender, initialSupply);
     }
 }
