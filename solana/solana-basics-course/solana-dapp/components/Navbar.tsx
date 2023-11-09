e25c94f8f4c020d1c2d532627d5771bab6fa8eef
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-11-09 10:11:11 -0800

    Update Navbar.tsx
    - Update z-index so it stays on top when scrolling

--- solana/solana-basics-course/solana-dapp/components/Navbar.tsx
+++ solana/solana-basics-course/solana-dapp/components/Navbar.tsx
@@ -8,7 +8,7 @@ const WalletMultiButtonDynamic = dynamic(
 
 const Navbar = () => {
   return (
-    <div className="w-full h-20 bg-gray-800 sticky top-0">
+    <div className="w-full h-20 bg-gray-800 sticky top-0 z-20">
       <div className="container mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
           Quick-Tac-Toe
