b31ada93ac4037c9bebbbcec33eb716a9aecc528
Sahil sen <sensahil25@gmail.com>
2024-08-03 04:54:13 +0530

    try 2

---
+++ Streams/wallet-allowance-checker-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WalletApprovalSearch from './components/WalletApprovalSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <WalletApprovalSearch />
+    </div>
+  );
+}
+
+export default App;
