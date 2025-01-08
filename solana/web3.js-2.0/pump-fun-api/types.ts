8cbb79e009509a1375ad541b79530d6872cd8e0f
amilz <85324096+amilz@users.noreply.github.com>
2025-01-08 08:26:11 -0800

    add pumpfun api types for sw3js2 (#89)

---
+++ solana/web3.js-2.0/pump-fun-api/types.ts
@@ -0,0 +1,85 @@
+import { 
+    Address, 
+    Commitment, 
+    Rpc, 
+    RpcSubscriptions, 
+    SolanaRpcApi, 
+    SolanaRpcSubscriptionsApi, 
+    TransactionMessageBytesBase64 
+} from "@solana/web3.js";
+
+type PumpFunType = 'BUY' | 'SELL';
+type PriorityFeeLevel = 'low' | 'medium' | 'high' | 'extreme';
+type TokenAmount = string | number;
+export type PumpFunRequest = PumpFunQuoteParams | PumpFunSwapParams;
+export type HttpRequestMethod = 'GET' | 'POST';
+export type PumpFunEndpoint = { 
+    path: string; 
+    method: HttpRequestMethod 
+};
+
+export interface PumpFunQuoteParams {
+    type: PumpFunType;
+    mint: Address;
+    amount: TokenAmount;
+}
+
+interface PumpFunQuoteMeta {
+    isCompleted: boolean;
+    outDecimals: number;
+    inDecimals: number;
+    totalSupply: string;
+    currentMarketCapInSol: number;
+}
+
+export interface PumpFunQuoteResponse {
+    quote: {
+        mint: Address;
+        bondingCurve: Address;
+        type: PumpFunType;
+        inAmount: string;
+        inAmountUi: number;
+        inTokenAddress: Address;
+        outAmount: string;
+        outAmountUi: number;
+        outTokenAddress: Address;
+        meta: PumpFunQuoteMeta;
+    }
+}
+
+export interface PumpFunSwapParams {
+    wallet: string;
+    type: PumpFunType;
+    mint: string;
+    inAmount: TokenAmount;
+    priorityFeeLevel?: PriorityFeeLevel;
+    slippageBps?: string;
+}
+
+export interface PumpFunSwapResponse {
+    tx: TransactionMessageBytesBase64;
+}
+
+interface AccountKey {
+    pubkey: Address;
+    isSigner: boolean;
+    isWritable: boolean;
+}
+
+interface SwapInstruction {
+    keys: AccountKey[];
+    programId: Address;
+    data: number[];
+}
+
+export interface PumpFunSwapInstructionsResponse {
+    instructions: SwapInstruction[];
+}
+
+export interface SignAndSendTransactionParams {
+    transactionBase64: string;
+    signerSecretKey: number[];
+    solanaRpc: Rpc<SolanaRpcApi>;
+    solanaRpcSubscriptions: RpcSubscriptions<SolanaRpcSubscriptionsApi>;
+    commitment?:Commitment;
+}
