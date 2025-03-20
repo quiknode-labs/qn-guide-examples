0df5c416e44c80d4c99947d7f4324dd99eb0f017
Sergen Uysal <0xsergen@gmail.com>
2025-03-20 18:44:56 +0300

    remove eslint warnings

--- sample-dapps/aml-and-cft-compliant-dapp/next-app/eslint.config.mjs
+++ sample-dapps/aml-and-cft-compliant-dapp/next-app/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
